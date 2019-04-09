5996706317e3d249dea0eb35d281f6bc7d6b241b
Jørgen Jacobsen <jorgeja@gmail.com>
2019-04-09 22:01:31 +0200

    cors

--- server.js
+++ server.js
@@ -40,6 +40,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use(cors(corsOptions));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 // parse application/json
 app.use(bodyParser.json());
