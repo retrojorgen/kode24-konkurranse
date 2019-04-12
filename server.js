f89e8b61abdadf21502926dbcedc4c0a1235128a
Jørgen Jacobsen <jorgeja@gmail.com>
2019-04-12 10:09:36 +0200

    allow posting

--- server.js
+++ server.js
@@ -44,6 +44,7 @@ app.use(cookieParser());
 app.use(cors(corsOptions));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
